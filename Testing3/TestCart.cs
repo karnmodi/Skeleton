8d7c2283e6f25e510057e6760367f70c9286b83a
Bassam <P2770348@my365.dmu.ac.uk>
2024-05-10 22:12:31 +0100

    TestCart Added

---
+++ Testing3/TestCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Testing3
+{
+    public class TestCart
+    {
+        public void InstanceOk()
+        {
+            clsCart ACart= new clsCart
+            Assertc,IsNotNull(ACart);
+        }
+        public void ActivePropertyOk()
+        {
+            clsCart ACart = new clsCart();
+            boolean TestData = true
+            ACart Active = TestData;
+            Assert.AEqual(ACart, Active, TestData); 
+                }
+}
