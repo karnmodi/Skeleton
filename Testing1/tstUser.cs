79cedd1c06434d4e24aa4916b94c3dcb596c2ecb
Karan Falgun Modi <84740993+karnmodi@users.noreply.github.com>
2024-05-10 22:15:43 +0100

    UserTest Created

---
+++ Testing1/tstUser.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsUser AUser = new clsUser;
+            Assert.IsNotNull(AUser);
+        }
+
+
+        [TestMethod]
+
+        public void ActivePropertyOK()
+        {
+            clsUser AUser = new clsUser();
+            Boolean TestData = true;
+            AUser.Active = TestData;
+            Assert.AreEqual(AUser.Active, TestData);
+        }
+    }
+}
