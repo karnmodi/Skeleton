1b405be43607dd1f06b91fa6876514c3355836bb
Karan Falgun Modi <84740993+karnmodi@users.noreply.github.com>
2024-05-11 21:42:51 +0100

    Testcases Created to check Properties and Get and Set Methods added in the clsProduct

--- ClassLibrary/clsProduct.cs
+++ ClassLibrary/clsProduct.cs
@@ -8,13 +8,139 @@ namespace ClassLibrary
 {
     public class clsProduct
     {
-        public string Title { get; set; }
-        public string Descripiton { get; set; }
-        public double Price { get; set; }
-        public string Condition { get; set; }
+        private int mProductID;
+        public int ProductID {
 
-        public string ImageURL { get; set; }
-        public DateTime DateListed { get; set; }
-        public bool Availability { get; set; }
+            get
+            {
+                return mProductID;
+            }
+
+            set
+            {
+                mProductID = value;
+            }
+                
+        }
+
+
+
+        private String mTitle;
+        public string Title
+        {
+
+            get
+            {
+                return mTitle;
+            }
+
+            set
+            {
+                mTitle = value;
+            }
+
+        }
+
+
+        private String mDescription;
+        public string Description
+        {
+
+            get
+            {
+                return mDescription;
+            }
+
+            set
+            {
+                mDescription = value;
+            }
+
+        }
+
+
+
+        private Double mPrice;
+        public Double Price
+        {
+
+            get
+            {
+                return mPrice;
+            }
+
+            set
+            {
+                mPrice = value;
+            }
+
+        }
+
+
+
+        private String mCondition;
+        public string Condition
+        {
+
+            get
+            {
+                return mCondition;
+            }
+
+            set
+            {
+                mCondition = value;
+            }
+
+        }
+
+
+
+        private bool mAvailability;
+        public bool Availability
+        {
+
+            get
+            {
+                return mAvailability;
+            }
+
+            set
+            {
+                mAvailability = value;
+            }
+
+        }
+
+
+        private DateTime mDateListed;
+        public DateTime DateListed
+        {
+
+            get
+            {
+                return mDateListed;
+            }
+
+            set
+            {
+                mDateListed = value;
+            }
+
+        }
+
+
+
+        public bool Find(int productID)
+        {
+            mProductID = 5;
+            mTitle = "ProductName";
+            mDescription = "Description1234";
+            mPrice = 5.50;
+            mCondition = "Good";
+            mAvailability = true;
+            mDateListed = Convert.ToDateTime("24/08/2024");
+            return true;
+        }
     }
 }
