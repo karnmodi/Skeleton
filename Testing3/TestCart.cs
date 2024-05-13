f2d01cc9bb6f1b652e9043b23ccf6372212486ac
Bassam <P2770348@my365.dmu.ac.uk>
2024-05-13 21:22:25 +0100

    testin

--- Testing3/TestCart.cs
+++ Testing3/TestCart.cs
@@ -9,16 +9,69 @@ namespace Testing3
 {
     public class TestCart
     {
-        public void InstanceOk()
+        [TestMethod]
+        public void TestMethod1()
         {
-            clsCart ACart= new clsCart
-            Assertc,IsNotNull(ACart);
+            clsCart ACart = new clsCart();
+            Assert.IsNotNull(ACart);
         }
+        [TestMethod]
         public void ActivePropertyOk()
         {
             clsCart ACart = new clsCart();
-            boolean TestData = true
-            ACart Active = TestData;
-            Assert.AEqual(ACart, Active, TestData); 
-                }
+            boolean TestData = true;
+            ACart.Active = TestData;
+            Assert.AreEqual(ACart.Active, TestData);
+        }
+
+        [TestMethod]
+        public void CartIDPropertyNotOk()
+        {
+            ClsCart ACart = new ClsCart();
+            int TestData = 1;
+            ACart.CartID = TestData;
+            Assert.AreEqual(ACart.CartID, TestData);
+        }
+
+        [TestMethod]
+        public void UserIDPropertyNotOK()
+        {
+            clsCart ACart = newclsCart();
+            int TestData = 3;
+            ACart.UserID = TestData;
+            Assert.AreEqual(ACart.UserID, TestData);
+        }
+
+        [TestMethod]
+        public void ProductIDPropertyNotOK()
+        {
+            clsCart ACart = newclsCart();
+            int TestData = 4;
+            ACart.ProductID = TestData;
+            Assert.AreEqual(ACart.ProductID, TestData);
+        }
+
+        [TestMethod]
+        public void QuantityPropertyNotOK()
+        {
+            clsCart ACart = newclsCart();
+            int TestData = 7;
+            ACart.Quantity = TestData;
+            Assert.AreEqual(ACart.Quantity, TestData);
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyNotOK()
+        {
+            clsCart ACart = newclsCart();
+            DateTime TestData = DateTime.Now.Date;
+            ACart.DateAdded = TestData;
+            Assert.AreEqual(ACart.DateAdded, TestData);
+        }
+    }
 }
+
+
+
+
+
