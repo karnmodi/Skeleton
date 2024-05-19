d6077e59d0ee857dd016414aba04c13c02e3e0a9
Diya Patel <p2760879@my365.dmu.ac.uk>
2024-05-19 21:52:53 +0100

    Practical 10 Completed

--- Testing2/tstReview.cs
+++ Testing2/tstReview.cs
@@ -1,12 +1,325 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using ClassLibrary;
+using System.Diagnostics;
 
 namespace Testing5
 {
     [TestClass]
     public class tstReview
     {
+
+        string Category = "Product A";
+        string Rating = "2";
+        string Comment = "Good value for money.";
+        string DateAdded = DateTime.Now.Date.ToString();
+        bool Recent = false;
+
+
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+
+        [TestMethod]
+        public void CategoryMinLessOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String Category = "";
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void CategoryMin()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String Category = "C";
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void CategoryMinPlusOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String Category = "CC";
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void CategoryMaxLessOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String Category = "";
+            Category = Category.PadRight(49 ,'C');
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryMax()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String Category = "";
+            Category = Category.PadRight(50, 'C');
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void CategoryMaxPlusOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String Category = "";
+            Category = Category.PadRight(51, 'C');
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryMid()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String Category = "";
+            Category = Category.PadRight(25, 'C');
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void CategoryExtremeMAX()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String Category = "";
+            Category = Category.PadRight(500, 'C');
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+
+        [TestMethod]
+        public void RatingExtremeMin()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = -500;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void RatingMinLessOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = 0;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void RatingMin()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = 1;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void RatingMinPlusOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = 2;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void RatingMaxLessOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = 9;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void RatingMax()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = 10;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void RatingMaxPlusOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = 11;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void RatingMid()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = 5;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void RatingExtremeMAX()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            Int32 value = 500;
+            string Rating = Convert.ToString(value);
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void RatingInvalidData()
+        {
+            clsReview AReview = new clsReview();
+            string Error = "";
+            string Rating = "5000sd";
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+
+
+
+        [TestMethod]
+        public void DatePostedExtremeMin()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(-100);
+            String DateAdded = TestDate.ToString();
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DatePostedMinLessOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddDays(-1);
+            String DateAdded = TestDate.ToString();
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void DatePostedMin()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            String DateAdded = TestDate.ToString();
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void DatePostedMinPlusOne()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddDays(1);
+            String DateAdded = TestDate.ToString();
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void DatePostedExtremeMAX()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(100);
+            String DateAdded = TestDate.ToString();
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+        [TestMethod]
+        public void DatePostedInvalidFormat()
+        {
+            clsReview AReview = new clsReview();
+            String Error = "";
+            String DateAdded = "This is not a DATE";
+            Error = AReview.Valid(Category, Rating, Comment, DateAdded, Recent);
+            Assert.AreEqual(Error, "");
+        }
+
+
+
+
+
         [TestMethod]
         public void InstanceOK()
         {
