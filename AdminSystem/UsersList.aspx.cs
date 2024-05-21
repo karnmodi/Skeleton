d12225d5ed47f40e0c721de87ee8f90c3cfa5c80
Samar Hayat <p2758630@my365.dmu.ac.uk>
2024-05-21 21:37:30 +0100

    Code Completed for the List

--- AdminSystem/UsersList.aspx.cs
+++ AdminSystem/UsersList.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,20 @@ public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            DisplayUserss();
+        }
 
+
+    }
+
+    void DisplayUserss()
+    {
+        clsUserCollection AllUserss = new clsUserCollection();
+        lstUsersList.DataSource = AllUserss.UserList;
+        lstUsersList.DataValueField = "UserID";
+        lstUsersList.DataTextField = "Username";
+        lstUsersList.DataBind();
     }
 }
