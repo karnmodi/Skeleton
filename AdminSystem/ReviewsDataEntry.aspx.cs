5df806d448a028bbc18c60a56ea6b9b7ee81089f
Diya Patel <p2760879@my365.dmu.ac.uk>
2024-05-29 13:01:53 +0100

    Week5 Diya

--- AdminSystem/ReviewsDataEntry.aspx.cs
+++ AdminSystem/ReviewsDataEntry.aspx.cs
@@ -8,9 +8,30 @@ using System.Web.UI.WebControls;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
+    Int32 ReviewID;
     protected void Page_Load(object sender, EventArgs e)
     {
+        ReviewID = Convert.ToInt32(Session["ReviewID"]);
+        if (IsPostBack == false)
+        {
+            if (ReviewID != -1)
+            {
+                DisplayReview();
+            }
+        }
+    }
 
+    void DisplayReview()
+    {
+        clsReviewCollection ReviewsAll = new clsReviewCollection();
+        ReviewsAll.ThisReview.Find(ReviewID);
+
+        txtReviewID.Text = ReviewsAll.ThisReview.ReviewID.ToString();
+        txtCategory.Text = ReviewsAll.ThisReview.Category.ToString();
+        txtRating.Text = ReviewsAll.ThisReview.Rating.ToString();
+        txtComment.Text = ReviewsAll.ThisReview.Comment.ToString();
+        txtDatePosted.Text = ReviewsAll.ThisReview.DatePosted.ToString();
+        chkRecent.Checked = ReviewsAll.ThisReview.Recent;
     }
 
     protected void btnFind_Click(object sender, EventArgs e)
@@ -37,24 +58,38 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void btnOK_Click(object sender, EventArgs e)
     {
         clsReview AReview = new clsReview();
+
+
         string Category = txtCategory.Text;
-        string Rating = Convert.ToString(txtRating.Text);
+        string Rating = txtRating.Text;
         string Comment = txtComment.Text;
         string DatePosted = txtDatePosted.Text;
-        Boolean Recent = chkRecent.Checked;
+        Boolean Recent = Convert.ToBoolean(chkRecent.Checked);
 
 
         string Error = "";
         Error = AReview.Valid(Category, Rating, Comment, DatePosted, Recent);
         if (Error == "")
         {
+            AReview.ReviewID = ReviewID;
             AReview.Category = Category;
             AReview.Rating = Convert.ToInt32(Rating);
             AReview.Comment = Comment;
             AReview.DatePosted = Convert.ToDateTime(DatePosted);
             AReview.Recent = Convert.ToBoolean(Recent);
-            Session["AReview"] = AReview;
-            Response.Redirect("ReviewsViewer.aspx");
+            clsReviewCollection ReviewList = new clsReviewCollection();
+            if (ReviewID == -1)
+            {
+                ReviewList.ThisReview = AReview;
+                ReviewList.Add();
+            }
+            else
+            {
+                ReviewList.ThisReview.Find(ReviewID);
+                ReviewList.ThisReview = AReview;
+                ReviewList.Update();
+            }
+            Response.Redirect("ReviewsList.aspx");
         }
         else
         {
