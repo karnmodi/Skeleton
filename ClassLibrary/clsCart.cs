7927c058c76256214628be7a8b905328030e66d4
Bassamkhan001 <p2770348@my365.dmu.ac.uk>
2024-06-04 23:24:00 +0100

    Validation Phase Completed.

--- ClassLibrary/clsCart.cs
+++ ClassLibrary/clsCart.cs
@@ -102,5 +102,84 @@ namespace ClassLibrary
             }
                 
         }
+
+        public string Valid(string cartDescription, string totalPrice, string quantity, string dateAdded, string active)
+        {
+            String Error = "";
+            Int32 sampleQuantity;
+            Int32 samplePrice;
+            DateTime sampleDate;
+            if (cartDescription.Length == 0)
+            {
+                Error += "Cart Description must not be empty.";
+            }
+            if (cartDescription.Length > 100)
+            {
+                Error += "Cart Description must be less than 100";
+            }
+
+            try
+            {
+                sampleQuantity = Convert.ToInt32(quantity);
+                if (sampleQuantity == 0)
+                {
+                    Error += "Quantity must not be zero.";
+                }
+                if (sampleQuantity < 0)
+                {
+                    Error += "Quantity must not be in negative format.";
+                }
+                if (sampleQuantity > 10000)
+                {
+                    Error += "Quantity must not be more than 10000";
+                }
+            }
+            catch
+            {
+                Error += "Quantity is not in correct format.";
+            }
+
+            try
+            {
+                samplePrice = Convert.ToInt32(totalPrice);
+                if (samplePrice == 0)
+                {
+                    Error += "Total Price must not be zero.";
+                }
+                if (samplePrice < 0)
+                {
+                    Error += "Total Price must not be in negative format.";
+                }
+                if (samplePrice > 15000)
+                {
+                    Error += "Total Price must not be more than 15000.";
+                }
+            }
+            catch
+            {
+                Error += "Total Price is not in correct format.";
+            }
+
+
+            try
+            {
+                sampleDate = Convert.ToDateTime(dateAdded);
+                
+                if (sampleDate < DateTime.Now.Date)
+                {
+                    Error += "Date must not be in the past.";
+                }
+                if (sampleDate > DateTime.Now.Date)
+                {
+                    Error += "Date must not be in the Future.";
+                }
+            }
+            catch
+            {
+                Error += "Added Date is not in the proper format.";
+            }
+
+            return Error;
+        }
     }
 }
